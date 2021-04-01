dc656e3b4ba8d7114b6a55b290b7882c9972cf48
kevinclee26 <kevin.c.lee26@gmail.com>
2021-03-31 21:59:42 -0700

    updated pipeline

--- fetch.py
+++ fetch.py
@@ -2,14 +2,16 @@ import requests
 import pymongo
 import time
 import secrets
-from geo_polygons import Polygons # Polygons holds the locality shapes
+# from geo_polygons import Polygons # Polygons holds the locality shapes
 import sys
 from Resources.companies import compInfo # compInfo contains info about companies
+import sqlalchemy
+from sqlalchemy.orm import Session
+from sqlalchemy.ext.automap import automap_base
 
-dc_track_id_path = 'Resources/census.json'
+# dc_track_id_path = 'Resources/census.json'
 # URL = 'https://maps2.dcgis.dc.gov/dcgis/rest/services/DCGIS_DATA/Transportation_WebMercator/MapServer/152/query?where=1%3D1&outFields=OBJECTID,AIRTEMP,RELATIVEHUMIDITY,VISIBILITY,WINDSPEED,DATADATETIME&outSR=4326&f=json'
-census_polygons=Polygons(dc_track_id_path)
-
+# census_polygons=Polygons(dc_track_id_path)
 
 # Initialize PyMongo to work with MongoDBs
 conn = 'mongodb://localhost:27017'
@@ -21,6 +23,17 @@ weather_docs=db.weather_docs
 scooter_docs=db.scooter_docs
 log=db.log
 
+# Initialize SQLAlchemy to work with Postgres
+conn = 'postgres://postgres:postgres@localhost:5432/scooters_DB'
+engine=sqlalchemy.create_engine(conn)
+
+# Define database and collection
+Base=automap_base()
+Base.prepare(engine, reflect=True)
+Completed_log=Base.classes.completed_log
+Current_fleet=Base.classes.current_fleet
+Removed=Base.classes.removed
+
 def get_bike_data(company_info):
     name=company_info['name']
     jobname=secrets.token_urlsafe(16)
@@ -30,7 +43,7 @@ def get_bike_data(company_info):
         try: 
             last_updated=response_json['last_updated']
         except: 
-            last_updated='NA'
+            last_updated=time.time()
         data=response_json
         for each_layer in company_info["layers"]:
             data = data[each_layer]
@@ -43,6 +56,7 @@ def get_bike_data(company_info):
                         'len': len(data), 
                         'status': 'success', 
                         'name': name, 
+                        'processed': False, 
                         'size': sys.getsizeof(data)})
     except:
         # If a company url doesn't work, continue next company url, print the url that is not work
@@ -52,6 +66,7 @@ def get_bike_data(company_info):
                         'name': name, 
                         'len': 0, 
                         'size': 0, 
+                        'processed': False, 
                         'last_updated': 'NA', 
                         'status': 'failed'}) 
     return 'complete'
@@ -59,7 +74,6 @@ def get_bike_data(company_info):
 def all_scooter_data(): 
     for each_company in compInfo: 
         print(each_company['name']+'-'+get_bike_data(each_company))
-    # print(one_company)
     return 'compete'
 
 def weather_data():
@@ -72,7 +86,7 @@ def weather_data():
         try: 
             last_updated=response_json['last_updated']
         except: 
-            last_updated='NA'
+            last_updated=time.time()
         for each_feature in data: 
             each_feature['jobname']=jobname
         weather_docs.insert_many(data)            
@@ -84,7 +98,6 @@ def weather_data():
                         'name': 'weather', 
                         'size': sys.getsizeof(data)})  
     except:
-        # If a company url doesn't work, continue next company url, print the url that is not work
         print("weather url has issue")
         log.insert_one({'time': time.time(), 
                         'jobname': jobname, 
@@ -94,101 +107,95 @@ def weather_data():
                         'name': weather, 
                         'status': 'failed'}) 
     return 'complete'
-    # weather_data=[each_feature['jobname']=jobname for each_feature in weather_data]
-    # doc_sets = process_weather_data(weather_data)
-
-
-    # Save "doc_sets" to MongoDB
-    # collection.insert_one({'data': doc_sets})
-    # collection.insert_many(doc_sets)
-    # if doc_sets: 
-    #     return True
-    # else: 
-    #     return False
-    # return len(data), sys.getsizeof(data)
-
-# def scooter_data(jobname=str(round(time.time(), 0))):
-#     all_data=[]
-#     for each_company in compInfo:
-#         name=each_company["name"]
-#         # print("$$$ ", name)
-#         try:
-#             response = requests.get(each_company["url"])
-#             data=response.json()
-#         except:
-#             # If a company url doesn't work, continue next company url, print the url that is not work
-#             print(f"{name} url has issue")
-#             continue
-#         # Assign the value of last_updated to varialbe "last_updated"
-#         # if data doesn't have last_updated attibute, use current time as "last_updated"
-#         # Make sure the last_updated is integer for future use
-#         # try:
-#         #     last_updated = int(data["last_updated"])
-#         # except:
-#         #     # 🍎Issue, using current time would save much more records, need modify later
-#         #     last_updated = int(time.time())
-
-#         # Retrieve data by layers
-#         for each_layer in each_company["layers"]:
-#             data = data[each_layer]
-#         for each_feature in data: 
-#             each_feature['jobname']=jobname
-#             each_feature['name']=name
-#         all_data=all_data+data
-#     scooter_docs.insert_many(all_data)
-#     return len(all_data), sys.getsizeof(all_data)
-
-#         # If last_updated changes, save data to mongoDB
-        # if last_updated > recent_last_updated[name]:
-            # Process data and save data to MongoDB
-            # doc_sets = process_data(data, name, last_updated)
-            # collection.insert_many(doc_sets)
-
-        # update recent_last_updated
-        # recent_last_updated[name] = last_updated
-
-# def process_weather_data(data): 
-#     doc_sets = []
-#     for each_record in data:
-#         lat = float(each_record["geometry"]["y"])
-#         lon = float(each_record["geometry"]["x"])
-#         point = [lon, lat]
-#         tractid = census_polygons.get_tract_id(point)
-
-#         new_doc = {}
-#         new_doc["air_temp"] = each_record["attributes"]["AIRTEMP"]
-#         new_doc["humidity"] = each_record["attributes"]["RELATIVEHUMIDITY"]
-#         new_doc["visibility"] = each_record["attributes"]["VISIBILITY"]
-#         new_doc["wind_speed"] = each_record["attributes"]["WINDSPEED"]
-#         new_doc["last_updated"] = int(int(each_record["attributes"]["DATADATETIME"]) / 1000)
-#         new_doc["tractid"] = tractid
-#         # new_doc["saved_at"] = int(time.time())
-
-#         doc_sets.append(new_doc)
-#     return doc_sets
-
-# def process_weather_data(data):
-#     polygons = Polygons()
-#     doc_sets = []
-#     for each_record in data:
-#         lat = float(each_record["geometry"]["y"])
-#         lon = float(each_record["geometry"]["x"])
-#         point = [lon, lat]
-#         tractid = polygons.get_tractid(point)
-
-#         new_doc = {}
-#         new_doc["air_temp"] = each_record["attributes"]["AIRTEMP"]
-#         new_doc["humidity"] = each_record["attributes"]["RELATIVEHUMIDITY"]
-#         new_doc["visibility"] = each_record["attributes"]["VISIBILITY"]
-#         new_doc["wind_speed"] = each_record["attributes"]["WINDSPEED"]
-#         new_doc["last_updated"] = int(int(each_record["attributes"]["DATADATETIME"]) / 1000)
-#         new_doc["tractid"] = tractid
-#         new_doc["saved_at"] = int(time.time())
-
-#         doc_sets.append(new_doc)
-
-#     return doc_sets
-
-
-
   
+def new_log_params(job_log): 
+    record_params={'time': job_log['time'], 
+         'jobname': job_log['jobname'], 
+         'last_updated': job_log['last_updated'], 
+         'len': job_log['len'], 
+         'status': job_log['status'], 
+         'name': job_log['name'], 
+         'size': job_log['size'], 
+         'processed_time': time.time(), 
+         'processed': True}
+    return record_params
+
+def update_existing_params(scooter, job_log): 
+    record_params={'lat': scooter['lat'], 
+                   'lon': scooter['lon'], 
+                   'last_updated': job_log['last_updated'], 
+                   'is_reserved': scooter.get('is_reserved', None), 
+                   'is_disabled': scooter.get('is_disabled', None), 
+                   'battery_level': scooter.get('battery_level', None), 
+                   'jobname': job_log['jobname'], 
+                   'new': False}
+    return record_params
+
+def new_scooter_params(scooter, job_log): 
+    record_params={'start_time': job_log['last_updated'], 
+                   'start_lat': scooter['lat'], 
+                   'start_lon': scooter['lon'], 
+                   'bike_id': scooter['bike_id'], 
+                   'company_name': job_log['name'], 
+                   'start_is_reserved': scooter.get('is_reserved', None), 
+                   'start_is_diabled': scooter.get('is_disabled', None), 
+                   'start_battery_level': scooter.get('battery_level', None), 
+                   'lat': scooter['lat'], 
+                   'lon': scooter['lon'], 
+                   'is_reserved': scooter.get('is_reserved', None),
+                   'is_disabled': scooter.get('is_disabled', None), 
+                   'battery_level': scooter.get('battery_level', None), 
+                   'last_updated': job_log['last_updated'], 
+                   'jobname': job_log['jobname'], #each_scooter['jobname'], 
+                   'new': True}
+    return record_params
+
+def process_job(job): 
+    session=Session(engine)
+    # find scooter record from Mongo
+    scooters_list=list(scooter_docs.find({'jobname': job['jobname']}))
+
+    # get list of current fleet from Postgres
+    current_fleet_list=session.query(Current_fleet).filter(Current_fleet.company_name==job['name']).all()
+    current_fleet_id_list=[scooter.bike_id for scooter in current_fleet_list]
+    fleet_list=current_fleet_id_list.copy()
+
+    # work on current
+    for each_scooter in scooters_list: 
+        current_id=each_scooter['bike_id']
+        current_index=current_fleet_id_list.index(current_id) if current_id in current_fleet_id_list else None
+        if not current_index==None: # if found then edit attributes  
+            scooter_rec=current_fleet_list[current_index] 
+            fleet_list.remove(current_id) # remove
+            scooter_params=update_existing_params(each_scooter, job)
+            for key, value in scooter_params.items(): 
+                setattr(scooter_rec, key, value)
+        else: # else new bike
+            scooter_params=new_scooter_params(each_scooter, job)
+            session.add(Current_fleet(**scooter_params))
+
+    # work on removed - get list of to be removed
+    removed_table_columns=sqlalchemy.inspect(Current_fleet).columns.keys()
+    remaining_fleet=session.query(Current_fleet).filter(Current_fleet.company_name==job['name']).filter(Current_fleet.bike_id.in_(fleet_list)).all()
+    for each_move_data in remaining_fleet: # iterate through left over
+        session.add(Removed(**{column: each_move_data.__dict__[column] for column in removed_table_columns}))
+        session.delete(each_move_data)
+
+    # process job
+    log_params=new_log_params(job)
+    session.add(Completed_log(**log_params))
+    session.commit()
+    
+    # update Mongo
+    log.remove({'_id': job['_id']})
+    scooter_docs.remove({'jobname': job['jobname']})
+
+    session.close()
+    return 'complete'
+
+def process_log():
+    all_jobs=list(log.find())
+    sorted_log=sorted(all_jobs, key=lambda log: log['time'])
+    for each_job in sorted_log: 
+        process_job(each_job)
+    return 'complete'
--- main.py
+++ main.py
@@ -3,12 +3,19 @@ import fetch
 
 sched=BlockingScheduler()
 fetch_freq_mins=5
+process_freq_mins=10
 
 @sched.scheduled_job('interval', minutes=fetch_freq_mins)
-def timed_job(): 
+def fetch_job(): 
 	fetch.all_scooter_data()
-	fetch.weather_data()
+	# fetch.weather_data()
 	print(f'This job is run every {fetch_freq_mins} minutes.')
+	return None
+
+@sched.scheduled_job('interval', minutes=process_freq_mins)
+def process_job():
+	fetch.process_log()
+	print(f'This job is run every {process_freq_mins} minutes.')
 	return None
 
 # @sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
